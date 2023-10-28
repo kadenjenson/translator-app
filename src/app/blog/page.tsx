71efbb5b0f7f59458628c8b9d1b108a1d5998494
Kaden <j.kaden.jenson@gmail.com>
2023-10-27 21:13:36 -0600

    added blog page

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export default function BlogPage()
+{
+    return (
+        <>
+            <h1>
+                <Link href="/">Back to Home Page</Link>
+            </h1>
+            <h1>Blog Page!</h1>
+        </>
+    );
+};

12b5baeac950f7e099372d1fe5e6c4d9f69ea3ef
Kaden <j.kaden.jenson@gmail.com>
2023-10-30 23:03:41 -0600

    updated blog page

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -3,11 +3,6 @@ import Link from "next/link";
 export default function BlogPage()
 {
     return (
-        <>
-            <h1>
-                <Link href="/">Back to Home Page</Link>
-            </h1>
-            <h1>Blog Page!</h1>
-        </>
+        <h1>Blog Page!</h1>
     );
 };

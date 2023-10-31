a79fd22ad8f5cda8ed7c1e208521f9ae65273676
Kaden <j.kaden.jenson@gmail.com>
2023-10-30 23:03:26 -0600

    added about and contact pages

---
+++ src/app/about/page.tsx
@@ -0,0 +1,8 @@
+import Link from "next/link";
+
+export default function AboutPage()
+{
+    return (
+        <h1>About Page!</h1>
+    );
+};
---
+++ src/app/contact/page.tsx
@@ -0,0 +1,8 @@
+import Link from "next/link";
+
+export default function ContactPage()
+{
+    return (
+        <h1>Contact Page!</h1>
+    );
+};

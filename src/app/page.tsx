7d8b9a554798487707fae3ec170c24e5882d3b19
Kaden <j.kaden.jenson@gmail.com>
2024-01-23 00:01:35 -0700

    testing tool containers on homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,19 @@
+import ToolContainer from '@/components/tools/ToolContainer'
+
 export default function Home()
 {
   return (
-    <h1>Home Page!</h1>
+    <div>
+      <h1>Home Page!</h1>
+
+      <ToolContainer />
+      <ToolContainer
+        title='and a 2nd one'
+        description='we can put lots of stuff in these'
+      />
+      <ToolContainer
+        title='last one'
+      />
+    </div>
   )
 }
